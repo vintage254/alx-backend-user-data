2b67c6981de1fb32fcbf224dc044df597f20c862
vintage254 <derricknjuguna414@gmail.com>
2024-09-11 21:11:15 +0000

    update password fix

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -147,16 +147,10 @@ class Auth:
             ValueError: if not found user
         """
         try:
-            # Find the user by reset token
             user = self._db.find_user_by(reset_token=reset_token)
-            if not user:
-                raise ValueError("User not found")
-            
-            # Hash the new password
             hashed_password = _hash_password(password)
-            
-            # Update user's password and reset token
-            self._db.update_user(user.id, hashed_password=hashed_password, reset_token=None)
-        
+            self._db.update_user(user.id,
+                    hashed_password=hashed_password,
+                    reset_token=None)
         except NoResultFound:
-            raise ValueError("User not found")
+            raise ValueError
